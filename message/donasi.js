f93e4c5686c870af09ff85b4676192ecb8d43539
saipulanuar <77055141+saipulanuar@users.noreply.github.com>
2021-12-29 10:36:40 +0700

    Update donasi.js

--- message/donasi.js
+++ message/donasi.js
@@ -5,7 +5,7 @@ return`
 │  Bantu Developer ngembangin bot ini
 │  dengan donasi seiklasnya 🙂
 │
-│  Shopeepay : 085156137901
+│  DANA : 088279268363
 │ 
 │ Makasih yang udah donasi
 │ dan bantu ngembangin bot ini
